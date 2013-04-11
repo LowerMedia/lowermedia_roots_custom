ae8f6bde73d11b33e6a14cb174f43a6ea0191517
hawkeye126 <pete.lower@gmail.com>
2013-04-11 08:36:48 -0600

    edit to footer

--- templates/footer.php
+++ templates/footer.php
@@ -11,5 +11,14 @@
     s.parentNode.insertBefore(g,s)}(document,'script'));
 </script>
 <?php endif; ?>
-
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try{
+var pageTracker = _gat._getTracker("UA-21432914-15");
+pageTracker._trackPageview();
+} catch(err) {}
+</script>
 <?php wp_footer(); ?>
