1ed4a5320b2dde3906c1baa7447718a76a6224e9
hawkeye126 <pete.lower@gmail.com>
2014-01-27 18:03:39 +0000

    adding adsense to header

--- templates/head.php
+++ templates/head.php
@@ -12,7 +12,7 @@
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
   <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/assets/js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
-
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
   <?php wp_head(); ?>
 
   <?php if (wp_count_posts()->publish > 0) : ?>
